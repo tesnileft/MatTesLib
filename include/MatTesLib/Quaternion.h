962ec3412e63330bac80afb2a39f4b119503a57c
tesni <tesnileft@gmail.com>
2025-08-14 01:23:28 +0200

    Barebones quaternion class

--- include/MatTesLib/Quaternion.h
+++ include/MatTesLib/Quaternion.h
@@ -1,14 +1,15 @@
-//
-// Created by tesni on 8/10/25.
-//
-
 #ifndef QUATERNION_H
 #define QUATERNION_H
-
+#include <vector>
 
 
 class Quaternion {
+    float a,b,c,d;
 
+public:
+    Quaternion();
+    Quaternion(float a[3]);
+    Quaternion(float x, float y, float z);
 };
 
 
